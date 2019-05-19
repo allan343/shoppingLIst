2371b47eb4dd714bf9fbad2b7d62ad5dfa7e2c32
Allan Cheng <allanhfcheng@gmail.com>
2019-05-18 18:10:46 -0700

    added default text after clicking on Add Item

--- index.js
+++ index.js
@@ -3,15 +3,10 @@
 
 $(function() {
     $('#js-shopping-list-form').submit(event => {
-      // this stops the default form submission behavior
-     
       event.preventDefault();
       const userTextElement = $(event.currentTarget).find('#shopping-list-entry');
-    
-      //console.log(userTextElement.val());
-
       $("ul").append(
-        `<li><span class="shopping-item">"  ${userTextElement.val()} "</span><div class="shopping-item-controls">
+        `<li><span class="shopping-item">  ${userTextElement.val()} </span><div class="shopping-item-controls">
         <button class="shopping-item-toggle">
             <span class="button-label">check</span></button>
                 <button class="shopping-item-delete">
@@ -19,6 +14,7 @@ $(function() {
                 </button>
               </div></li>`
         );
+        userTextElement.val("");
     });  
 
 })
